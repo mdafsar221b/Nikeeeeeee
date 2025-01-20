a2fc40e20b53675f3415cab9564e7d3aa0c53f19
Md Afsar <afsarsayyyed@gmail.com>
2025-01-20 22:34:31 +0530

    Make Componant for each sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { CustomerReviews,Footer,Hero,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality } from "./sections";
+
+const App=()=> (
+  <main className="relative ">
+  {/* <nav/>  */}
+  <section className="x1:padding-1 wide:padding-r padding-b">
+    <hero/> 
+  </section>
+  <section className="padding">
+    <PopularProducts/>
+  </section>
+  <section className="padding">
+    <SuperQuality/>
+  </section>
+  <section className="padding-x py-10">
+    <Services/>
+  </section>
+  <section className="padding">
+    <SpecialOffer/>
+  </section>
+  <section className="bg-pale-blue padding ">
+    <CustomerReviews/>
+  </section>
+  <section className="paddind-x sm:py-32 py-16 w-full">
+    <Subscribe/>
+  </section>
+  <section className="bg-black padding-x padding-t pb-8">
+    <Footer/>
+  </section>
+
+  </main>
+)
+
+export default App;
