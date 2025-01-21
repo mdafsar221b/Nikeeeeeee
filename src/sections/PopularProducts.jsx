7274a440446018f4443c6344fcf8ecb873983a20
Md Afsar <afsarsayyyed@gmail.com>
2025-01-22 00:05:11 +0530

    Done and dusted with SuperQuality Section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -5,15 +5,15 @@ const PopularProducts = () => {
       <section id="products"
        className="max-container max-sm:mt-12">
         <div className='flex flex-col justify-start gap-5'>
-          <h2 className="text-4xl font-palanquin font-bold ">Our
-            <span className="text-coral-red">
-              Popular Product
+          <h2 className="text-4xl font-palanquin font-bold "> Our
+            <span className="text-coral-red ml-3">
+              Popular Products
               </span>
           </h2>
           <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray ">Experience Top notch quality and style with our sought-after selections.Doscover a world of comfort,design,and value</p>
 
         </div>
-        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
           {products.map((product) => (
 
             <PopularProductCard key={product.name}
